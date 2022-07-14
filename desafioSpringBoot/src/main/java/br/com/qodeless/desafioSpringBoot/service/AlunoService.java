9dca5e12edf79fb580974429016a1412e756a797
MatheusPanassolOliveira <90454538+MatheusPanassolOliveira@users.noreply.github.com>
2022-07-14 12:52:15 -0300

    Add files via upload

---
+++ desafioSpringBoot/src/main/java/br/com/qodeless/desafioSpringBoot/service/AlunoService.java
@@ -0,0 +1,30 @@
+package br.com.qodeless.desafioSpringBoot.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.qodeless.desafioSpringBoot.model.Aluno;
+import br.com.qodeless.desafioSpringBoot.repository.AlunoRepository;
+
+@Service
+public class AlunoService {
+	
+	@Autowired
+	private AlunoRepository repository;
+	
+	public List<Aluno> buscarAlunos(){
+		return repository.findAll();
+		
+	}
+	
+	public Aluno criarAluno(Aluno aluno){
+		return repository.save(aluno);
+	}
+
+	public Aluno buscarPorId(Integer id) {
+        return repository.findById(id).get();
+    }
+}
